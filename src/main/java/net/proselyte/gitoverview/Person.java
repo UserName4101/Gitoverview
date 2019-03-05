7275e5637c3f0bc441b472100bc9d9f0517c8496
Username4101 <Faumos1995@yandex.ru>
2019-03-05 16:39:05 +0400

    Adding class Person.java

---
+++ src/main/java/net/proselyte/gitoverview/Person.java
@@ -0,0 +1,24 @@
+package net.proselyte.gitoverview;
+
+public class Person extends BaseEntity{
+
+    private String firstName;
+
+    private String lastName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+}
